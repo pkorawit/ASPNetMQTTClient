d0b00f8ede123b8a3ad9e39fab8dc44319c32458
developer <developer@DESKTOP-QJ18DK4>
2017-02-21 11:26:43 +0700

    Dynamically display sensor value

---
+++ MqttWebClient/Dashboard.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Dashboard.aspx.cs" Inherits="MqttWebClient.Dashboard" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">        
+        <asp:ScriptManager ID="ScriptManager1" runat="server" />
+        <asp:UpdatePanel runat="server" id="UpdatePanel">
+            <ContentTemplate>
+                <asp:Button runat="server" id="Button1" text="Connect" OnClick="Button1_Click" />
+                <asp:Timer runat="server" id="Timer2" Interval="5000" OnTick="Timer2_Tick"></asp:Timer>
+                <br>
+                Tempurature 1 :
+                <asp:Label runat="server" id="Sensor1" BackColor="#FFCCFF" /><br>                             
+                Humidity 1 :
+                <asp:Label runat="server" id="Sensor2" BackColor="#66FF33" />                
+            </ContentTemplate>
+        </asp:UpdatePanel>
+    </form>
+</body>
+</html>
---
+++ MqttWebClient/Dashboard.aspx.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+namespace MqttWebClient
+{
+    public partial class Dashboard : System.Web.UI.Page
+    {
+        MqttClient client = new MqttClient("broker.hivemq.com");
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            byte code = client.Connect(Guid.NewGuid().ToString());
+            ushort msgId = client.Subscribe(new string[] { "keppa", "/iottest" },
+            new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE,
+                         MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE });
+
+            client.MqttMsgPublishReceived += client_MqttMsgPublishReceived;
+
+            Sensor1.Text = "0.00";
+            Sensor2.Text = "0.00";
+            Session["T1"] = "0.00";
+            Session["H1"] = "0.00";
+        }
+
+        void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+        {
+            if (e != null)
+            {
+                if (e.Topic.Equals("keppa"))
+                {
+                    var msg = Encoding.UTF8.GetString(e.Message);
+
+                    if (msg.Contains(","))
+                    {
+                        string[] msgArray = msg.Split(',');
+
+                        if (msgArray[0].Equals("T1"))
+                            Session["T1"] = msgArray[1];
+
+                        if (msgArray[0].Equals("H1"))
+                            Session["H1"] = msgArray[1];
+                    }
+                }
+            }
+        }
+
+        protected void Timer2_Tick(object sender, EventArgs e)
+        {
+            Sensor1.Text = Session["T1"].ToString();
+            Sensor2.Text = Session["H1"].ToString();
+        }
+    }
+}
